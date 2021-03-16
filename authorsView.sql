8197cd611ba23eba4a72c86636377d13920bc4af
grsl <grslong@gmail.com>
2021-03-16 16:59:11 +0000

    Split out view.

---
+++ authorsView.sql
@@ -0,0 +1,16 @@
+/*
+|----------------------------------
+| Structure for view `vAuthors`
+|----------------------------------
+*/
+    CREATE VIEW `vAuthors` AS
+        SELECT
+            CONCAT(
+                IF((`authors`.`givenname` IS NOT NULL), CONCAT(`authors`.`givenname`,' '),'')
+               ,IF((`authors`.`initials`  IS NOT NULL), CONCAT(`authors`.`initials`,' '),'')
+               ,`authors`.`familyname`) AS `Name`
+               ,`psuedonym` AS "Psuedonym"
+               ,`yob`       AS "Year of Birth"
+               ,`yod`       AS "Year of Death"
+               ,`biog`      AS "Short Biography"
+           FROM `authors`;
