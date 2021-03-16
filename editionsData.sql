13c0b720fed066480ee5abc73cfeb3e4673ac4d0
grsl <grslong@gmail.com>
2021-03-16 17:00:42 +0000

    Split out data.

---
+++ editionsData.sql
@@ -0,0 +1,17 @@
+/*
+|---------------------------------------
+| Data for the `editions` table.
+|---------------------------------------
+*/
+    INSERT INTO `editions` (`edition`)
+    VALUES ("First")
+          ,("Second")
+          ,("Third")
+          ,("Fourth")
+          ,("Fifth")
+          ,("Sixth")
+          ,("Seventh")
+          ,("Eighth")
+          ,("Ninth")
+          ,("Tenth")
+          ;
